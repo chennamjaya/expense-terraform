9685b3b9cf19def837068cb45c53fddae1604016
chennamjaya <chennamjaya@gmail.com>
2023-11-25 00:43:49 +0530

    Added route to default and peering

--- main.tf
+++ main.tf
@@ -8,6 +8,7 @@ module "vpc" {
   default_vpc_id = var.default_vpc_id
   account_no = var.account_no
   default_vpc_cidr = var.default_vpc_cidr
+  default_route_table_id = var.default_route_table_id
 }
 
  
