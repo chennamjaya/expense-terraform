9235e8c352c1ed153003ce27f6e8acd07f75b122
chennamjaya <chennamjaya@gmail.com>
2023-11-24 23:48:10 +0530

    Added peering connection

--- main.tf
+++ main.tf
@@ -5,6 +5,8 @@ module "vpc" {
   private_subnets = var.private_subnets
   public_subnets = var.public_subnets
   azs = var.azs
+  default_vpc_id = "vpc-0f85d7a6071a32e51	"
+  account_no = "851512651356"
 }
 
  
