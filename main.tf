cba22c1a3d0eaec6d74094ccf3d04434a3d7b72e
chennamjaya <chennamjaya@gmail.com>
2023-11-26 21:05:55 +0530

    uncommented private lb

--- main.tf
+++ main.tf
@@ -21,15 +21,15 @@ module "vpc" {
 #  vpc_id = module.vpc.vpc_id
 #}
 
-#module "private_lb" {
-#  source = "./modules/alb"
-#  alb_sg_allow_cidr = var.vpc_cidr
-#  alb_type = "private"
-#  env = var.env
-#  internal = true
-#  subnets = module.vpc.private_subnets
-#  vpc_id = module.vpc.vpc_id
-#}
+module "private_lb" {
+  source = "./modules/alb"
+  alb_sg_allow_cidr = var.vpc_cidr
+  alb_type = "private"
+  env = var.env
+  internal = true
+  subnets = module.vpc.private_subnets
+  vpc_id = module.vpc.vpc_id
+}
 
 #module "frontend" {
 #  source = "./modules/app"
