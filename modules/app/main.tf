fca3e4c562071d12119d97b3c13ee4167a678c8e
chennamjaya <chennamjaya@gmail.com>
2023-11-26 16:23:55 +0530

    Added instance profile from role

--- modules/app/main.tf
+++ modules/app/main.tf
@@ -70,8 +70,13 @@ resource "aws_iam_role" "role" {
 
 
   tags = {
-    tag-key = "tag-value"
+    tag-key = "${var.env}-${var.component}-role"
   }
+}
+
+resource "aws_iam_instance_profile" "instance_profile" {
+  name = "${var.env}-${var.component}-role"
+  role = aws_iam_role.role.name
 }
 
 resource "aws_launch_template" "template" {
@@ -80,7 +85,7 @@ resource "aws_launch_template" "template" {
   instance_type = var.instance_type
   vpc_security_group_ids = [aws_security_group.security_group.id]
   iam_instance_profile {
-    name = aws_iam_role.role.name
+    name = aws_iam_instance_profile.instance_profile.name
   }
   user_data = base64encode(templatefile("${path.module}/userdata.sh", {
     role_name = var.component
